129526e3a86fc7560fecca510ed154b421b2aec3
hts <hans.tschan@gmail.com>
2025-07-29 10:29:04 +0200

    Add ESLint configuration and ignore files for frontend

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended'
+  ],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    requireConfigFile: false,
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {
+    // Add custom rules here if needed
+  }
+}
